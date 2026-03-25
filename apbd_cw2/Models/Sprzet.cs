785f48d79790db25fc342be34cf7a6f57eed1db0
Alicja Choda <s34446@pjwstk.edu.pl>
2026-03-25 20:06:29 +0100

    dodanie klasy sprzęt

---
+++ apbd_cw2/Models/Sprzet.cs
@@ -0,0 +1,17 @@
+﻿namespace apbd_cw2.Models; 
+
+public abstract class Sprzet
+{
+    
+    private static int _nextId = 1;
+    
+    public string Nazwa { get; set; }
+    
+    public bool CzyDostepny { get; set; } = true;
+
+    Sprzet(String nazwa1)
+    {
+        Nazwa = nazwa1;
+    }
+
+}
