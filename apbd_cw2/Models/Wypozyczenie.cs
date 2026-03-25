efdd0d315768a389d9f274e4782a088fd04d5c91
Alicja Choda <s34446@pjwstk.edu.pl>
2026-03-25 21:57:38 +0100

    Stworzenie klasy Wypozyczenie

---
+++ apbd_cw2/Models/Wypozyczenie.cs
@@ -0,0 +1,34 @@
+﻿namespace apbd_cw2.Models;
+
+public class Wypozyczenie
+{
+    private static int _nextId = 1;
+    public int Id { get; private set; } = _nextId++;
+    
+    public Uzytkownik KtoWypozycza { get; private set; }
+    public Sprzet WypozyczonySprzet { get; private set; }
+    
+    public DateTime DataWypozyczenia { get; private set; }
+    public DateTime KiedyZwrot { get; private set; }
+    
+    public DateTime? DataFaktycznegoZwrotu { get; private set; } 
+    
+    //? - null
+    
+    public Wypozyczenie(Uzytkownik Osoba, Sprzet sprzet, int IleDni)
+    {
+        KtoWypozycza = Osoba;
+        
+        WypozyczonySprzet = sprzet;
+        
+        DataWypozyczenia = DateTime.Now;
+        
+        KiedyZwrot = DateTime.Now.AddDays(IleDni);
+    }
+
+
+    public void OznaczZwrocone()
+    {
+        DataFaktycznegoZwrotu = DateTime.Now;
+    }
+}
